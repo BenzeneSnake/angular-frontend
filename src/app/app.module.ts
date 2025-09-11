e204ec10f1d2f6b60f65e67800f0c92ebf52c318
BenzeneSnake <kelly5142200@gmail.com>
2025-09-11 21:45:11 +0800

    feat:新增login頁面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntryModule } from './entry/entry.module';
 import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, EntryModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    EntryModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,9 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { BasePageComponent } from '../shared/base/base-page';
-
-gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'lib-app-entry',
