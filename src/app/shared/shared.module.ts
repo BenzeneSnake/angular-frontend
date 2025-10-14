081f1407bbdf531c91ab36bca4c36dda7be4145c
BenzeneSnake <kelly5142200@gmail.com>
2025-10-14 11:22:18 +0800

    feat:前端接口開發-新增form-error-msg.component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormErrorMsgComponent } from './component/form-error-msg/form-error-msg.component';
 import { MenuHeaderComponent } from './component/menu-header/menu-header/menu-header.component';
 
 const components = [MenuHeaderComponent];
 
 @NgModule({
-  declarations: [...components],
-  imports: [],
-  exports: [...components],
+  declarations: [...components, FormErrorMsgComponent],
+  imports: [CommonModule],
+  exports: [...components, FormErrorMsgComponent],
 })
 export class SharedModule {}
